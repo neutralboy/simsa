35965d998d96ab28655be15b461bba84847ece1d
neutralboy <leopoorna99@gmail.com>
2022-01-06 08:54:31 +0530

    Create privacy_policy.js

---
+++ pages/privacy_policy.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+const PrivacyPolicy = () => (
+<div class="c3">
+        <p class="c0"><span class="c1">Privacy policy </span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">Data Protection Notice</span></p>
+        <p class="c0">
+            <span class="c1">
+                One of your rights under Indian law is that you must be informed when your personal data - also known as personal information - is processed (collected, used, stored) by any organisation. You also have the right to know the
+                details and purpose of that processing.
+            </span>
+        </p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0">
+            <span class="c1">
+                Some of the services offered on our website require the processing of your personal data. In such cases, links are available on those pages directing you to the specific information about the processing contained in the
+                relevant data protection notices.
+            </span>
+        </p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">Here we give you a general overview of some of the ways this website processes your personal data</span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">Your personal data and our website</span></p>
+        <p class="c0"><span class="c1">We collect your personal information only to the extent necessary to fulfil a precise purpose related to our tasks</span></p>
+        <p class="c0"><span class="c1">We will use your personal data for direct marketing purposes</span></p>
+        <p class="c0"><span class="c1">We do not reuse the information for another purpose that is different to the one stated</span></p>
+        <p class="c0"><span class="c1">We put in place measures to guarantee that your data are kept up-to-date and processed securely</span></p>
+        <p class="c0">
+            <span class="c1">
+                Under certain conditions outlined in law, we may disclose your information to third parties, (such as the European Anti-Fraud Office, the Court of Auditors, or law enforcement authorities) if it is necessary and
+                proportionate for lawful, specific purposes
+            </span>
+        </p>
+        <p class="c0"><span class="c1">You have the right to access your personal information, to have it corrected and the right to recourse; at any time if you believe your data protection rights have been breached</span></p>
+        <p class="c0"><span class="c1">We do not keep your personal information for longer than necessary for the purposes for which we collected it</span></p>
+        <p class="c0"><span class="c1">However, we may keep your information for a longer period for historical, statistical or scientific purposes with the appropriate safeguards in place</span></p>
+        <p class="c0"><span class="c1">Cookies</span></p>
+        <p class="c0"><span class="c1">Our site does not use any cookies.</span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">Social Media</span></p>
+        <p class="c0"><span class="c1">We use social media to present our work through widely used and contemporary channels. Our use of social media is highlighted on this website.</span></p>
+        <p class="c0"><span class="c1">For instance, you can watch videos, which we upload to our YouTube page and follow links from our website to Google, Twitter, Facebook and LinkedIn.</span></p>
+        <p class="c0">
+            <span class="c1">
+                Cookies are not set by our display of social media buttons to connect to those services when our website pages are loaded on your computer (or other devices) or from components from those services embedded in our web pages.
+            </span>
+        </p>
+        <p class="c0"><span class="c1">Each social media channel has its policy on the way they process your personal data when you access their sites.</span></p>
+        <p class="c0">
+            <span class="c1">
+                For example, if you choose to watch one of our videos on YouTube, you will be asked for explicit consent to accept YouTube cookies; if you look at our Twitter activity on Twitter, you will be asked for explicit consent to
+                accept Twitter cookies; the same applies for Google, Facebook and LinkedIn.
+            </span>
+        </p>
+        <p class="c0"><span class="c1">If you have any concerns or questions about their use of your personal data, you should read their privacy policies carefully before using them.</span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">Complaint handling: what kinds of personal data does the website process?</span></p>
+        <p class="c0">
+            <span class="c1">We process the personal data submitted by website visitors. This will include names, contact details as well as the content of the allegations (insofar as the latter qualify as personal data).</span>
+        </p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">Who may receive my personal data?</span></p>
+        <p class="c0"><span class="c1">The case file containing your personal data is accessible to relevant members of our staff all access to case files is logged.</span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">What are my rights?</span></p>
+        <p class="c0">
+            <span class="c1">
+                You are entitled to access the personal data we hold about you and to have them rectified where necessary. In certain cases, you also have the right to have your data blocked or erased or to object to their processing. For
+                more information, please see Articles 13 to 19 of the Regulation. To exercise any of these rights, please contact us using our contact form. We will reply within three months. Please note that in some cases restrictions
+                under Article 20 of the Regulation may apply.
+            </span>
+        </p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">How long do you keep my data?</span></p>
+        <p class="c0"><span class="c1">We keep log files - including your personal data - for up to two years</span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0"><span class="c1">Who can I contact?</span></p>
+        <p class="c0"><span class="c1">You can contact us as follows:e-mail:info@simsa.co.in;</span></p>
+        <p class="c0"><span class="c1">You can find complete contact information on the website contact page.</span></p>
+        <p class="c0"><span class="c1">Contact Us</span></p>
+        <p class="c0"><span class="c1">Email Address</span></p>
+        <p class="c0"><span class="c1">director@simsa.co.in</span></p>
+        <p class="c0"><span class="c1">Privacy Policy</span></p>
+        <p class="c0"><span class="c1">Terms and Conditions</span></p>
+        <p class="c0"><span class="c1">All Rights Reserved 2021-Present SIMSA</span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+        <p class="c0 c2"><span class="c1"></span></p>
+ </div>
+);
+
+export default PrivacyPolicy;
